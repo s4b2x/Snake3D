c7d7cf7b6da39b1dfb69ab5960c9749f866ac455
sebu10n <s.schulz@tgs-somborn.de>
2014-08-14 02:53:56 +0200

    Menue inkl Highscore und mehr
    getHighscore.php und postAndGetHighscore.php zum Eintragen und Abrufen
    des Highscores aus der mysql-Tabelle auf meinem Webspace
    
    index.html umbenannt in index1.0.html
    neue index.html mit Umleitung auf index4.0.html
    
    Einbindung von JQuery
    
    Schlangenkopf erkennbar
    
    etliche Bugfixes

---
+++ getHighscore.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'login.php';
+
+
+$userName = $_POST['userName'];
+$score = $_POST['score'];
+$date = date("Y-m-d H:i:s");
+
+$con = mySQL_connect($db_hostname, $db_username, $db_passwd);
+
+if(!$con){
+    die('Fehler bei der Verbindung zum MySQL-Server!');
+}
+
+if(!mySQL_select_db('sauronsql1', $con)){
+    die('Fehler bei der Auswahl der Datenbank!');
+}
+
+function ausgabe(){
+    $sql = "SELECT * FROM `snakehighscore` ORDER BY `snakehighscore`.`score` DESC LIMIT 0, 15 ";
+    $result = mysql_query($sql)
+            or die("SELECT fehlgeschlagen ".mysql_error()." ..");
+
+    $zeile;
+    $platz = 1;
+    while($zeile = mysql_fetch_array($result)){
+        echo '
+<tr>
+    <th>' . $platz . '</th>
+    <th>' . $zeile['nickname'] . '</th>
+    <th>' . $zeile['score'] . '</th>
+    <th>' . $zeile['datum'] . '</th>
+</tr>
+';
+        $platz++;
+    }
+}
+
+ausgabe();
+
+?>
