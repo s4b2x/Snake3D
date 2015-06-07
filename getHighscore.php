7f986e2edeb3a3175642acd683d80a17b64dda24
sebu10n <g.sebix@gmail.com>
2015-06-07 18:25:50 +0200

    db-Name, komprimierter Highscore
    DB-Name nach Server-Umstellung angepasst
    im Highscore werden per SQL nun gleiche Namen zusammengefasst, Datum
    passt noch nicht.

--- getHighscore.php
+++ getHighscore.php
@@ -1,22 +1,29 @@
 <?php
 require_once 'login.php';
 
+
+$userName = $_POST['userName'];
+$score = $_POST['score'];
+$date = date("Y-m-d H:i:s");
+
 $con = mySQL_connect($db_hostname, $db_username, $db_passwd);
 
 if(!$con){
     die('Fehler bei der Verbindung zum MySQL-Server!');
 }
 
-if(!mySQL_select_db('sauronsql1', $con)){
+if(!mySQL_select_db('sebix_db1', $con)){
     die('Fehler bei der Auswahl der Datenbank!');
 }
 
 function ausgabe(){
-    $sql = "SELECT * FROM `snakehighscore` ORDER BY `snakehighscore`.`score` DESC LIMIT 0, 15 ";
+/*    $sql = "SELECT * FROM `snakehighscore` ORDER BY `snakehighscore`.`score` DESC LIMIT 0, 15 ";  */
+    $sql = "SELECT nickname, MAX(score) AS `score`, COUNT(*) AS `rounds`, datum FROM `snakehighscore` GROUP BY `nickname` ORDER BY MAX(score) DESC LIMIT 15";
     $result = mysql_query($sql)
             or die("SELECT fehlgeschlagen ".mysql_error()." ..");
     
-    echo '<tr>  <td>#</td>  <td>Name</td>  <td>Punkte</td>  <td>Schwierigkeitsgrad</td>  <td>Datum</td>  </tr>';
+/*    echo '<tr>  <td>#</td>  <td>Name</td>  <td>Punkte</td>  <td>Schwierigkeitsgrad</td>  <td>Datum</td>  </tr>';  */
+    echo '<tr>  <td>#</td>  <td>Name</td>  <td>Punkte</td>  <td>Runden</td>  <td>Datum</td>  </tr>';
     
     $zeile = null;
     $platz = 1;
@@ -26,10 +33,11 @@ function ausgabe(){
     <th>' . $platz . '</th>
     <th>' . $zeile['nickname'] . '</th>
     <th>' . $zeile['score'] . '</th>
-    <th>' . $zeile['speed'] . '</th>
+    <th>' . $zeile['rounds'] . '</th>
     <th>' . $zeile['datum'] . '</th>
 </tr>
 ';
+/*  <th>' . $zeile['speed'] . '</th>    */
         $platz++;
     }
 }
